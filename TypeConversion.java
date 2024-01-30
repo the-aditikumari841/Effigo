818bbeb362e0a03bb3690972bc17e94665ba70f6
aditi.kumari@effigo.tech <aditi.kumari@effigo.tech>
2024-01-30 17:04:42 +0530

    first commit

---
+++ TypeConversion.java
@@ -0,0 +1,19 @@
+
+public class TypeConversion {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int ants = 2000; 
+		int purchasedBlockOfCheese = 1;
+		System.out.println("There are" + ants + "ants, and" + purchasedBlockOfCheese + "block of cheese" );
+		System.out.println("Each ant carried " + ((double)purchasedBlockOfCheese)/ants + "from the total cheese amount");
+		double salary = 5423.94;
+		int roundedSalary = (int)salary;
+		System.out.println("Mortgage Broker : What is your monthly salary in dollars?");
+		System.out.println("My monthly salary is : " + roundedSalary);
+	}
+
+}
+
+
