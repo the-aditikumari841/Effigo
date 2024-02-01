ff7dfb37c20cd84820598a7bffa7412f7eca3f68
aditi.kumari@effigo.tech <aditi.kumari@effigo.tech>
2024-02-01 16:27:52 +0530

    commit

---
+++ HighestElement.java
@@ -0,0 +1,24 @@
+public class HighestElement 
+{
+
+    public static void main(String args[])
+    {
+        Scanner scanner = new Scanner(System.in);
+
+        int number1 = scanner.nextInt();
+        int number2 = scanner.nextInt();
+        int number3 = scanner.nextInt();
+
+        int highest = Integer.MIN_VALUE();
+
+        if(number 1 > highest)
+            highest = number1;
+        else if(number2 > highest)
+            highest = number2;
+        else
+            highest = number3;
+
+        System.out.println("Highest = " + highest);
+    }
+    
+}
