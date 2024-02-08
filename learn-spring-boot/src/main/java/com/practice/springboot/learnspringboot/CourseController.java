638b0c83d47011c3ce7cc8bf40fde88ec3344646
Aditi Kumari <aditikumari8421@gmail.com>
2024-02-08 21:33:55 +0530

    commit

--- learn-spring-boot/src/main/java/com/practice/springboot/learnspringboot/CourseController.java
+++ learn-spring-boot/src/main/java/com/practice/springboot/learnspringboot/CourseController.java
@@ -1,5 +1,23 @@
 package com.practice.springboot.learnspringboot;
 
+import java.util.Arrays;
+import java.util.List;
+
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+
 public class CourseController {
+	
+	@RequestMapping("/courses")
+	public List<Course> retrieveAllCourses(){
+		return Arrays.asList(
+				new Course(1, "Learn SpringBoot" , "eclipse"),
+				new Course(2, "Learn SpringBoot" , "eclipse")
+				);
+	}
 
 }
+
