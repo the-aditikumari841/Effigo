22ebc7eefe2ccfcc656ce47586004a926b6a2244
Aditi Kumari <aditikumari8421@gmail.com>
2024-02-09 11:45:39 +0530

    commit

--- learn-spring-boot/src/main/java/com/practice/springboot/learnspringboot/CurrencyConfigurationController.java
+++ learn-spring-boot/src/main/java/com/practice/springboot/learnspringboot/CurrencyConfigurationController.java
@@ -1,23 +1,18 @@
 package com.practice.springboot.learnspringboot;
 
-import java.util.Arrays;
-import java.util.List;
-
-
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RestController
 public class CurrencyConfigurationController {
-	
+
 	@Autowired
 	private CurrencyServiceConfiguration configuration;
-	
+
 	@RequestMapping("/currency-configuration")
-	public CurrencyServiceConfiguration retrieveAllCourses(){
+	public CurrencyServiceConfiguration retrieveAllCourses() {
 		return configuration;
 	}
 
 }
-
