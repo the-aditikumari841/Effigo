2775ea430c0d5d39be78d09fb9d9210f200f2e02
unknown <aditikumari8421@gmail.com>
2024-02-15 17:39:50 +0530

    new commit

---
+++ learningportalproject/src/main/java/com/learningportal/learningportalproject/controller/CourseCategoryController.java
@@ -0,0 +1,46 @@
+package com.learningportal.learningportalproject.controller;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.web.bind.annotation.*;
+
+import com.learningportal.learningportalproject.entity.CourseCategoryEntity;
+import com.learningportal.learningportalproject.service.CourseCategoryService;
+
+
+@RestController
+@RequestMapping("/employee")
+public class CourseCategoryController {
+	
+	private final CourseCategoryService coursecategoryService;
+	
+	public CourseCategoryController(CourseCategoryService coursecategoryService) {
+		this.coursecategoryService = coursecategoryService;
+	}
+	
+	@GetMapping
+	public List<CourseCategoryEntity> findAllCourseCategory() {
+		return coursecategoryService.findAllCourseCategory();
+	}
+	
+	@GetMapping("/{id}")
+	public Optional<CourseCategoryEntity> findEmployeeById(@PathVariable("id") String courseCategory){
+		return coursecategoryService.findById(courseCategory);
+	}
+	
+	@PostMapping
+	public CourseCategoryEntity saveEmployee(@RequestBody CourseCategoryEntity coursecategoryEntity) {
+		return coursecategoryService.saveCourseCategory(coursecategoryEntity);
+	}
+	
+	@PutMapping
+	public CourseCategoryEntity updateCourseCategory(@RequestBody CourseCategoryEntity coursecategoryEntity) {
+		return  coursecategoryService.updateCourseCategory(coursecategoryEntity);
+	}
+	
+	@DeleteMapping("/{id}")
+	public void deleteCourseCategory(@PathVariable("id") String courseCategory) {
+		coursecategoryService.deleteCourseCategory(courseCategory);
+	}
+}
---
+++ learningportalproject/src/main/java/com/learningportal/learningportalproject/controller/UserController.java
@@ -0,0 +1,5 @@
+package com.learningportal.learningportalproject.controller;
+
+public class UserController {
+
+}
