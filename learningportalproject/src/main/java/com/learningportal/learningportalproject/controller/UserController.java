278ae27fc72fab02606a791715086b75443e341f
unknown <aditikumari8421@gmail.com>
2024-02-16 10:39:38 +0530

    new commit

--- learningportalproject/src/main/java/com/learningportal/learningportalproject/controller/UserController.java
+++ learningportalproject/src/main/java/com/learningportal/learningportalproject/controller/UserController.java
@@ -1,5 +1,49 @@
 package com.learningportal.learningportalproject.controller;
 
-public class UserController {
+import java.util.List;
+import java.util.Optional;
 
+import org.springframework.web.bind.annotation.*;
+import com.learningportal.learningportalproject.entity.UserEntity;
+import com.learningportal.learningportalproject.service.UserService;
+
+
+
+
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+	
+	private final UserService userService;
+	
+	public UserController(UserService userService) {
+		this.userService = userService;
+	}
+	
+	@GetMapping
+	public List<UserEntity> findAllUser() {
+		return userService.findAllUser();
+	}
+	
+	@GetMapping("/{id}")
+	public Optional<UserEntity> findUserById(@PathVariable("id") Long userID){
+		return userService.findById(userID);
+	}
+	
+	@PostMapping
+	public UserEntity saveUser(@RequestBody UserEntity userEntity) {
+		return userService.saveUser(userEntity);
+	}
+	
+	@PutMapping
+	public UserEntity updateUser(@RequestBody UserEntity userEntity) {
+		return  userService.updateUser(userEntity);
+	}
+	
+	@DeleteMapping("/{id}")
+	public void deleteUser(@PathVariable("id") Long userID) {
+		userService.deleteUser(userID);
+	}
 }
+
