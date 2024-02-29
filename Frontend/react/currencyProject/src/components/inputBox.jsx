813730b9eecddc3bfa1166560ca5ae21c743e274
Aditi Kumari <aditikumari8421@gmail.com>
2024-02-29 14:44:08 +0530

    new commit

--- Frontend/react/currencyProject/src/components/inputBox.jsx
+++ Frontend/react/currencyProject/src/components/inputBox.jsx
@@ -1,19 +1,28 @@
-import React from 'react'
+import React from "react";
 
 function inputBox(
-    label,
-    amount,
-    onAmountChange,
-    onCurrencyChange,
-    currencyOptions = [], 
-    selectedcurrency = "usd",
-    amountDisabled = false,
-    currencyDisabled = false ,
-    className = ""
+  label,
+  amount,
+  onAmountChange,
+  onCurrencyChange,
+  currencyOptions = [],
+  selectedcurrency = "usd",
+  amountDisabled = false,
+  currencyDisabled = false,
+  className = ""
 ) {
   return (
-    <div>inputBox</div>
-  )
+    <div className={`bg-white p-3 rounded-lg text-sm flex ${className}`}>
+      <div className="w-1-2">
+        <label className="text-black/40 mb-2 inline block">{label}</label>
+        <input
+          type="number"
+          className="outline-none w-full bg-transparent py-1.5"
+          placeholder="Amount"
+        />
+      </div>
+    </div>
+  );
 }
 
-export default inputBox
+export default inputBox;
